513c7a1280041bd93316d2d9326095bf4b48e230
Luis Pérez <luisperez@Luiss-MacBook-Pro.local>
2017-04-05 20:56:44 -0500

    init com

---
+++ main.py
@@ -0,0 +1,33 @@
+import pyglet 
+
+game_window = pyglet.window.Window(600, 400)
+
+background = pyglet.image.load('background.png')
+back = pyglet.sprite.Sprite(background)
+
+#back.queue(background)
+
+caraimg = pyglet.image.load_animation('cara.gif')
+cara = pyglet.sprite.Sprite(caraimg, x = 5, y = 40)
+
+
+#music = pyglet.media.load('Bounce.wav')
+#music.play(-1,0.0)
+
+
+@game_window.event
+def on_draw():
+	game_window.clear()
+	back.draw()
+	cara.draw()
+
+#def on_
+
+def update(dt):
+    
+    back.x += dt * -100
+
+
+pyglet.clock.schedule_interval(update, 1/60.)
+
+pyglet.app.run()
